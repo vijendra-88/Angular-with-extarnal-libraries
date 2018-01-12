be4cc27b50f5b0a029bb2cbfd3d34e5458337462
vijendra-88 <35370592+vijendra-88@users.noreply.github.com>
2018-01-12 17:56:03 +0530

    Add angular 4 files

---
+++ Angular 4/ClientApp/app/app.module.ts
@@ -0,0 +1,87 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Route } from '@angular/router';
+import { DataTableModule, SharedModule, PaginatorModule, InputTextModule, ButtonModule, DialogModule } from 'primeng/primeng';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+///import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './components/app/app.component'; 
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { GPSVehicleDataComponent } from './components/GPSVehicleData/GPSVehicleData.Component';
+import { LoginComponent } from './components/Login/Login.Component';
+import { LayoutComponent } from './components/Layout/Layout.Component';
+import { DashboardComponent } from './components/Dashboard/Dashboard.Component';
+import { NotFoundComponent } from './components/NotFound/NotFound.Component';
+import { RegisterComponent } from './components/Register/Register.Component';
+import { RoleComponent } from './components/Role/Role.Component';
+
+
+import { AlertService, AuthenticationService, UserService, AuthGuard, RoleService } from './Services/Index';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        LoginComponent,
+        GPSVehicleDataComponent,
+        LayoutComponent,
+        DashboardComponent,
+        RoleComponent,
+        HomeComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        RoleService,
+        UserService
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        InputTextModule,
+        DataTableModule,
+        SharedModule,
+        PaginatorModule,
+        BrowserAnimationsModule,
+        //AngularFontAwesomeModule,
+        ButtonModule,
+        DialogModule,
+        RouterModule.forRoot([
+            //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+
+            {
+                path: 'Dashboard', component: LayoutComponent,
+                children: [{ path: '', component: DashboardComponent }]
+            },
+            {
+                path: 'home', component: LayoutComponent,
+                children: [{ path: '', component: HomeComponent }]
+            },
+            {
+                path: 'register', component: LayoutComponent,
+                children: [{ path: '', component: RegisterComponent }]
+            },
+            {
+                path: 'role', component: LayoutComponent,
+                children: [{ path: '', component: RoleComponent }]
+            },
+
+            {
+                path: 'GPS-Vehicle-Data', component: LayoutComponent,
+                children: [{ path: '', component: GPSVehicleDataComponent }]
+            },
+
+            { path: '**', redirectTo: 'NotFoundComponent' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
---
+++ Angular 4/Startup.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using MPOTrackingApp.Helpers;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+
+namespace MPOTrackingApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Old
+            services.AddSingleton(_ => Configuration);
+            services.AddMvc();
+
+
+
+            //services.AddCors();
+            //services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("TestDb"));
+            //services.AddMvc();
+            //services.AddAutoMapper();
+
+            //// configure strongly typed settings objects
+            //var appSettingsSection = Configuration.GetSection("AppSettings");
+            //services.Configure<AppSettings>(appSettingsSection);
+
+            //// configure jwt authentication
+            //var appSettings = appSettingsSection.Get<AppSettings>();
+            //var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer(x =>
+            //{
+            //    x.RequireHttpsMetadata = false;
+            //    x.SaveToken = true;
+            //    x.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(key),
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false
+            //    };
+            //});
+
+            //// configure DI for application services
+            //services.AddScoped<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //Old
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+
+
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            //// global cors policy
+            //app.UseCors(x => x
+            //    .AllowAnyOrigin()
+            //    .AllowAnyMethod()
+            //    .AllowAnyHeader()
+            //    .AllowCredentials());
+
+            //app.UseAuthentication();
+
+            //app.UseMvc();
+        }
+    }
+}
